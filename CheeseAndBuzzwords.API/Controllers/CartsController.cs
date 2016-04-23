dfc2a1c8804c017c4139b374c49bf28753444f97
Joe Lloyd <josephglloyd@gmail.com>
2016-04-23 17:59:29 +0100

    Add stub API for carts

---
+++ CheeseAndBuzzwords.API/Controllers/CartsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+
+namespace CheeseAndBuzzwords.API.Controllers
+{
+	[RoutePrefix("carts")]
+	public class CartsController : ApiController
+	{
+		[HttpGet]
+		public IHttpActionResult Get()
+		{
+			return Ok(new[] {"Cart1", "Cart2"});
+		}
+	}
+}
